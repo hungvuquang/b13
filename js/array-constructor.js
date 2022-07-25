76609232d86e5e05a69862b46cdf6fdd9aee47e8
hungvuquang <hungvq@outlook.com>
2022-07-25 20:13:37 +0700

    Cú pháp cơ bản trong Javacsript
    Cú pháp cơ bản trong Javacsript

---
+++ js/array-constructor.js
@@ -0,0 +1,6 @@
+var colors=new array('while','black','custom');
+
+var el=document.getElementById('colors');
+el.textContent=colors[0];
+
+
---
+++ js/update-array.js
@@ -0,0 +1,12 @@
+// Create the array an assign it values
+var color=['while', 'black', 'custom'];
+
+// Update the third item in the array
+color[2]='baige';
+
+// Get the element with an id of colors
+var el=document.getElementById(elementld: 'color');
+
+//Replace element with third item from the array
+el.textContent=color[2];
+
